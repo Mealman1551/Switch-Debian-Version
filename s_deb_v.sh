29a2ac5d15b410dea73c760717f4026f1484d709
Mealman1551 <nathandubuy4@gmail.com>
2025-01-17 18:41:15 +0100

    Update s_deb_v.sh

--- s_deb_v.sh
+++ s_deb_v.sh
@@ -38,50 +38,6 @@ deb http://deb.debian.org/debian/ ${codename}-updates main contrib non-free non-
 deb-src http://deb.debian.org/debian/ ${codename}-updates main contrib non-free non-free-firmware
 EOF
     fi
-
-    update_os_release_and_version "$codename"
-}
-
-update_os_release_and_version() {
-    local codename=$1
-
-    case $codename in
-        "bookworm")
-            sudo tee /etc/os-release > /dev/null <<EOF
-PRETTY_NAME="Debian GNU/Linux 12 (bookworm)"
-VERSION_ID="12"
-VERSION="12 (bookworm)"
-ID=debian
-EOF
-            echo "12" | sudo tee /etc/debian_version > /dev/null
-            ;;
-        "trixie")
-            sudo tee /etc/os-release > /dev/null <<EOF
-PRETTY_NAME="Debian GNU/Linux 13 (trixie)"
-VERSION_ID="13"
-VERSION="13 (trixie)"
-ID=debian
-EOF
-            echo "13" | sudo tee /etc/debian_version > /dev/null
-            ;;
-        "testing")
-            sudo tee /etc/os-release > /dev/null <<EOF
-PRETTY_NAME="Debian GNU/Linux Testing"
-VERSION="Testing"
-ID=debian
-EOF
-            echo "testing" | sudo tee /etc/debian_version > /dev/null
-            ;;
-        "sid")
-            sudo tee /etc/os-release > /dev/null <<EOF
-PRETTY_NAME="Debian GNU/Linux (sid)"
-VERSION_ID="sid"
-VERSION="Unstable"
-ID=debian
-EOF
-            echo "sid" | sudo tee /etc/debian_version > /dev/null
-            ;;
-    esac
 }
 
 echo "Which version of Debian do you want to use?"
